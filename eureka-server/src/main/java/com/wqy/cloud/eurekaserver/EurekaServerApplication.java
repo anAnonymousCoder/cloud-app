5d85617fde18dd190d55b87f80c68d19c7434ebf
CodePeasantry <2445034472@qq.com>
2020-09-29 09:24:25 +0800

    beta version

---
+++ eureka-server/src/main/java/com/wqy/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.wqy.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * register-center-app
+ * @author wqy
+ * @version 1.0 2020/9/28
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
